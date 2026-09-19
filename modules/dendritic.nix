a993cefe6b889d5ef9d0bb2a28e1344f80ed127e
whipplash <mattsnoopy2@gmail.com>
2026-09-19 11:45:58 -0400

    testing major release lockstep

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -9,6 +9,8 @@
     den.url = "github:denful/den";
     flake-file.url = "github:vic/flake-file";
 
+    nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
