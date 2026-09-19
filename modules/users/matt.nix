f903c7f222530d477a3060d655bb31b3c1b7da94
whipplash <mattsnoopy2@gmail.com>
2026-09-19 11:02:50 -0400

    Package import?

--- modules/users/matt.nix
+++ modules/users/matt.nix
@@ -7,7 +7,57 @@
       den.batteries.primary-user
     ]; # (10)
     homeManager = { pkgs, ... }: {
-      home.packages = [ pkgs.vim ];
+      home.packages = with pkgs; [
+        alacritty
+        alacritty-theme
+        #bambu-studio
+        brightnessctl
+        btop
+        chafa
+        cmatrix
+        claude-code
+        github-copilot-cli
+        fastfetch
+        feh
+        ffmpeg
+        fish
+        #freecad
+        fzf
+        gimp
+        giph
+        grc
+        grim
+        haskellPackages.sixel
+        htop
+        inkscape
+        kitty-themes
+        libsixel
+        libvirt
+        lsd
+        lsof
+        neovim
+        nwg-look
+        obsidian
+        playerctl
+        prismlauncher
+        qalculate-qt
+        qmk
+        ripgrep
+        slurp
+        showmethekey
+        sops
+        spotify
+        starship
+        tree
+        via
+        virt-manager
+        virt-viewer
+        wev
+        wf-recorder
+        wlr-randr
+        yamlfmt
+        yamllint
+      ];
     };
   };
 }
