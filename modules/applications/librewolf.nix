5514a4b4b051c6678f2ff35a9177db22e4f02482
Matthew Kenkel <mattsnoopy2@gmail.com>
2026-09-19 17:52:56 -0400

    Correct format

--- modules/applications/librewolf.nix
+++ modules/applications/librewolf.nix
@@ -1,14 +1,20 @@
-{den, ...}: {
-  den.aspects.librewolf = {
-    nixos = {pkgs, ...}: {
-      programs.librewolf = {
-        enable = true;
-        globalExtensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          privacy-badger
-          uBlock-origin
-          stylus
-        ];
-      };
+{den, ...}: let
+  librewolfHomeManager = {config, ...}: {
+    programs.librewolf = {
+      enable = true;
+      globalExtensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        privacy-badger
+        {
+          package = ublock-origin;
+          settings = {
+            private_browsing = true;
+          };
+        }
+      ];
     };
+  };
+in {
+  den.aspects.librewolf = {
+    homeManager = librewolfHomeManager;
   };
 }
