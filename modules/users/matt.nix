f3201d29e53ad0b1f7e6bda5206c214402155699
whipplash <mattsnoopy2@gmail.com>
2026-09-19 11:59:39 -0400

    ...Unfree that easy?

--- modules/users/matt.nix
+++ modules/users/matt.nix
@@ -5,6 +5,7 @@
       den.batteries.define-user
       den.batteries.primary-user
       (den.batteries.user-shell "fish")
+      (den.batteries.unfree ["obsidian" "spotify"])
     ]; # (10)
     homeManager = {pkgs, ...}: {
       home.packages = with pkgs; [
